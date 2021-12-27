be413967ee618cf09767de2c57a00a9b5a2c2cb7
jakam <jaka.malezic@gmail.com>
2021-12-27 21:58:52 +0100

    Added bassuc functions

--- app/src/main/java/com/example/uvustoreapp2/MainActivity.java
+++ app/src/main/java/com/example/uvustoreapp2/MainActivity.java
@@ -4,11 +4,84 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView product;
+    private String url = "https://uvustore.azurewebsites.net/api/ProductsApi";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        product = (TextView) findViewById(R.id.product);
     }
+
+    public  void showProduct(View view){
+        if (view != null){
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response){
+            ArrayList<String> data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject object =response.getJSONObject(i);
+                    String name = object.getString("name");
+                    String price = object.getString("price");
+                    String dateadded = object.getString("date Added");
+
+                    data.add(name + " " + price + " " + dateadded);
+
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+
+                }
+            }
+
+            product.setText("");
+
+
+            for (String row: data){
+                String currentText = product.getText().toString();
+                product.setText(currentText + "\n\n" + row);
+            }
+
+        }
+
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+
+
 }
